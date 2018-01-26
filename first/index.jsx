094e76595e13f02bba9383fd34da6a725b3163d5
maxim <mymailforxperia@gmail.com>
2018-01-26 16:15:35 +0300

    first

---
+++ first/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class First extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <div>WOWOW</div>
+            </div>
+        );
+    }
+}
+
+export default First;
